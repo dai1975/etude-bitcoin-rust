8d37fd10b0447d0662ea4e9118c7a9ce1d1b6a5f
KANDA Daisuke <dai1975@gmail.com>
2016-03-27 15:56:33 +0900

    impl addr message

---
+++ src/protocol/message/addr.rs
@@ -0,0 +1,27 @@
+use std;
+extern crate time;
+use super::Address;
+use ::serialize::{self, Serializable};
+
+#[derive(Debug,Default)]
+pub struct AddrMessage {
+   pub addrs : Vec<Address>,
+}
+impl std::fmt::Display for AddrMessage {
+   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+      write!(f, "Addr(len={})", self.addrs.len())
+   }
+}
+
+impl Serializable for AddrMessage {
+   fn get_serialize_size(&self) -> usize {
+      self.addrs.get_serialize_size()
+   }
+   fn serialize(&self, io:&mut std::io::Write) -> serialize::Result {
+      self.addrs.serialize(io)
+   }
+   fn unserialize(&mut self, io:&mut std::io::Read) -> serialize::Result {
+      self.addrs.unserialize(io)
+   }
+}
+
