3022d15a96d899e9d3d59e0dfe3371006ac5f9c2
KANDA Daisuke <dai1975@gmail.com>
2016-05-07 01:26:21 +0900

    debug uint256 string format

--- src/primitive/block_header.rs
+++ src/primitive/block_header.rs
@@ -1,8 +1,6 @@
 use std;
 use super::{Error,UInt256,pow,ConsensusParams};
 use ::serialize::{self, Serializable};
-extern crate crypto;
-use self::crypto::digest::Digest;
 
 #[derive(Debug,Default,Clone)]
 pub struct BlockHeader {
@@ -18,31 +16,6 @@ impl std::fmt::Display for BlockHeader {
    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
       write!(f, "BlockHeader(version={}, prev={}, merkle={}, time={}, bits={}, nonce={})",
              self.version, self.hash_prev_block, self.hash_merkle_root, self.time, self.bits, self.nonce)
-   }
-}
-
-impl BlockHeader {
-   pub fn calc_hash(&self) -> UInt256 {
-      let serpara = serialize::SerializeParam::new_net();
-      let mut mem = &mut [0u8; 80];
-      let _ = self.serialize(&mut &mut mem[..], &serpara);
-
-      let mut hasher = crypto::sha2::Sha256::new();
-      let out = &mut [0u8; 32];
-      hasher.input(mem);
-      hasher.result(out);
-      hasher.reset();
-      hasher.input(out);
-      hasher.result(out);
-
-      UInt256::new(out)
-   }
-
-   pub fn check(&self, params:&ConsensusParams) -> Result<(), Error> {
-      try!(pow::check_proof_of_work(&self.calc_hash(), self.bits, params));
-
-      // TODO: timestamp check
-      Ok(())
    }
 }
 
@@ -72,3 +45,54 @@ impl Serializable for BlockHeader {
    }
 }
 
+impl BlockHeader {
+   pub fn calc_hash(&self) -> UInt256 {
+      let o:&Serializable = self as &Serializable;
+      o.serialize_hash256d(&serialize::SerializeParam::new_net()).unwrap()
+   }
+   
+   pub fn check(&self, params:&ConsensusParams) -> Result<(), Error> {
+      try!(pow::check_proof_of_work(&self.calc_hash(), self.bits, params));
+
+      // TODO: timestamp check
+      Ok(())
+   }
+}
+
+#[test]
+fn test_hash() {
+   use primitive::UInt256;
+   use serialize::SerializeParam;
+
+   //block 125552 https://blockexplorer.com/block/00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d
+   let h = BlockHeader {
+      version: 1,
+      hash_prev_block:  UInt256::from_str("00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81").unwrap(),
+      hash_merkle_root: UInt256::from_str("2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3").unwrap(),
+      time:  0x4dd7f5c7,
+      bits:  0x1a44b9f2,
+      nonce: 0x9546a142,
+   };
+   let ser = &SerializeParam::new_net();
+   
+   {
+      assert_eq!(80usize, h.get_serialize_size(ser));
+      let mut mem:Vec<u8> = Vec::with_capacity(h.get_serialize_size(ser));
+      assert_eq!(80usize, h.serialize(&mut mem, ser).unwrap());
+      let expect:[u8;80] = [
+         0x01, 0x00, 0x00, 0x00,
+         0x81, 0xcd, 0x02, 0xab, 0x7e, 0x56, 0x9e, 0x8b, 0xcd, 0x93, 0x17, 0xe2, 0xfe, 0x99, 0xf2, 0xde,
+         0x44, 0xd4, 0x9a, 0xb2, 0xb8, 0x85, 0x1b, 0xa4, 0xa3, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0xe3, 0x20, 0xb6, 0xc2, 0xff, 0xfc, 0x8d, 0x75, 0x04, 0x23, 0xdb, 0x8b, 0x1e, 0xb9, 0x42, 0xae,
+         0x71, 0x0e, 0x95, 0x1e, 0xd7, 0x97, 0xf7, 0xaf, 0xfc, 0x88, 0x92, 0xb0, 0xf1, 0xfc, 0x12, 0x2b,
+         0xc7, 0xf5, 0xd7, 0x4d,
+         0xf2, 0xb9, 0x44, 0x1a,
+         0x42, 0xa1, 0x46, 0x95,
+      ];
+      assert_eq!(&expect[..], mem.as_slice());
+   }
+
+   let hash = h.calc_hash();
+   let hashstr = format!("{}", hash);
+   assert_eq!("00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d", hashstr);
+}
